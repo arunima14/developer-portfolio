c1cea607b57fde0132383f9b8cd7c4e6665b94e5
Arunima Singh <arunimaforwork@gmail.com>
2023-12-13 23:54:27 +0530

    Update projectsData.js

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,83 +1,101 @@
-import one from "../assets/svg/projects/one.svg";
-import two from "../assets/svg/projects/two.svg";
-import three from "../assets/svg/projects/three.svg";
-import four from "../assets/svg/projects/four.svg";
-import five from "../assets/svg/projects/five.svg";
-import six from "../assets/svg/projects/six.svg";
+import jobPortal from "../assets/svg/projects/job-portal.svg";
+import LetsChat from "../assets/svg/projects/chat-app.svg";
+import anchaal from "../assets/svg/projects/aanchal.svg";
+import bookRecommender from "../assets/svg/projects/book-recommender.svg";
+import covid19Resources from "../assets/svg/projects/covid19-resources.svg";
+import funEdu from "../assets/svg/projects/fun-edu.svg";
+import notesKepper from "../assets/svg/projects/notes-keeper.svg";
+import productsList from "../assets/svg/projects/product-list.svg";
+import quizify from "../assets/svg/projects/quizify.svg";
 
 export const projectsData = [
   {
     id: 1,
-    projectName: "Emergency VITConnect",
-    projectDesc:
-      "A web application that will help the students to get the best emergency medical assistance at university.",
-    tags: ["React", "Node", "Firebase", "MongoDB"],
-    code: "https://github.com/sankalp20/Emergency-VITConnect",
-    demo: "https://github.com/sankalp20/Emergency-VITConnect",
-    image: one,
+    projectName: "Job Portal",
+    projectDesc: 
+      "React.js job portal with Bootstrap for streamlined job search and applications.",
+    tags: ["React", "Redux", "Bootstrap"],
+    code: "https://github.com/arunima14/Job-Portal",
+    demo: "https://job-portal-arunima14.vercel.app/",
+    image: jobPortal,
   },
   {
     id: 2,
+    projectName: "Lets Chat",
+    projectDesc:
+      "Chat application with the integration of Chat GPT API for dynamic responses.",
+    tags: ["React", "Redux", "ChatEngine"],
+    code: "https://github.com/arunima14/LetsChat",
+    demo: "https://lets-chat-kohl.vercel.app/",
+    image: LetsChat,
+  },
+  {
+    id: 3,
+    projectName: "Products Shop",
+    projectDesc:
+      "A React.js web page showcasing products with filtering, sorting, and pagination features.",
+    tags: ["React", "HTML/CSS", "Node"],
+    code: "https://github.com/arunima14/Product-Lists/",
+    demo: "https://product-lists-iota.vercel.app/",
+    image: productsList,
+  },
+  {
+    id: 4,
+    projectName: "Quiz App",
+    projectDesc:
+      "A dynamic quiz app that tracks user answers with a timer and progress bar, and displaying personalized results.",
+    tags: ["HTML/CSS", "Vanilla js"],
+    code: "https://github.com/arunima14/Quiz-App",
+    demo: "https://quiz-app-arunima14.vercel.app/",
+    image: quizify,
+  },
+  {
+    id: 5,
     projectName: "FunEdu: Mental HealthMate",
     projectDesc:
       "A React-based web application to assess the mental health of teenagers.",
     tags: ["React", "Node", "JavaScript"],
     code: "https://github.com/arunima14/fun-edu",
     demo: "https://github.com/arunima14/fun-edu",
-    image: two,
+    image: funEdu,
   },
   {
-    id: 3,
+    id: 6,
     projectName: "Book Recommender System",
     projectDesc:
       "Recommends us top 25books from our database and also recommends on basis of search",
     tags: ["Python", "Jupyter Notebook", "Datasets"],
     code: "https://github.com/arunima14/book-recommender-system-python",
     demo: "https://github.com/arunima14/book-recommender-system-python",
-    image: three,
+    image: bookRecommender,
   },
   {
-    id: 4,
+    id: 7,
     projectName: "Anchaal app",
     projectDesc:
       "A Women Safety app with unique future of connectivity in zero network state across all devices.",
     tags: ["React", "Firebase"],
     code: "https://github.com/arunima14/Anchaal-app",
     demo: "https://github.com/arunima14/Anchaal-app",
-    image: four,
+    image: anchaal,
   },
   {
-    id: 5,
+    id: 8,
     projectName: "Covid-19 resources",
     projectDesc:
       "It is a website that provides all the resources related to Covid-19.",
     tags: ["Javascript", "HTML/CSS", "Firebase"],
     code: "https://github.com/arunima14/covid-19-resources",
     demo: "https://github.com/arunima14/covid-19-resources",
-    image: five,
+    image: covid19Resources,
   },
   {
-    id: 6,
+    id: 9,
     projectName: "Note Keeper",
     projectDesc: "A simple webpage to add your notes",
     tags: ["React", "Firebase"],
     code: "https://github.com/arunima14/Keeper-Notes",
     demo: "https://github.com/arunima14/Keeper-Notes",
-    image: six,
+    image: notesKepper,
   },
 ];
-
-// Do not remove any fields.
-// Leave it blank instead as shown below
-
-/* 
-{
-    id: 1,
-    projectName: 'Car Pooling System',
-    projectDesc: '',
-    tags: ['Flutter', 'React'],
-    code: '',
-    demo: '',
-    image: ''
-}, 
-*/
