aef9ddbc1d61458774f104f3bdb192aafe9a98fd
Arunima Singh <arunimaforwork@gmail.com>
2023-12-14 01:09:52 +0530

    updated resume

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,4 +1,4 @@
-import resume from "../assets/pdf/resume.pdf";
+import resume from "../assets/pdf/Arunima_Singh_SDE_Resume.pdf";
 import userImage from "../assets/png/userImage.png";
 
 export const headerData = {
