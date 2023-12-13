6543267fa9730d5192d47391c499f8891240870c
Arunima Singh <arunimaforwork@gmail.com>
2023-12-13 23:53:53 +0530

    Updated experienceData.js

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,37 +1,30 @@
 export const experienceData = [
+  // {
+  //   id: 1,
+  //   company: "GoComet",
+  //   jobtitle: "Backend Intern",
+  //   startYear: "Jan, 2024",
+  //   endYear: "Present",
+  // },
   {
-    id: 1,
+    id: 2,
     company: "Microsoft Corporation India",
     jobtitle: "SDE Intern",
     startYear: "May, 2023",
     endYear: "June, 2023",
   },
   {
-    id: 2,
+    id: 3,
     company: "Microsoft Engage",
     jobtitle: "Mentee",
     startYear: "May 2022",
     endYear: "June 2023",
   },
   {
-    id: 3,
+    id: 4,
     company: "The Visual Cinema",
     jobtitle: "Content writer",
     startYear: "May 2021",
     endYear: "June 2021",
-  },
-  {
-    id: 4,
-    company: "D2CIgnitersClub VITBhopal",
-    jobtitle: "Core Team Member",
-    startYear: "April 2022",
-    endYear: "May 2023",
-  },
-  {
-    id: 5,
-    company: "TechLearn.live",
-    jobtitle: "Campus AmbassadorCampus Ambassador",
-    startYear: "May 2021",
-    endYear: "Dec 2021",
   },
 ];
